6b98dd8b95ac593afa042c253caa67b1988ffd59
Rene Verbeek <rjdverbeek@gmail.com>
2019-02-14 19:50:48 +0100

    Improved the flight logger

--- plugins/flight_logger.py
+++ plugins/flight_logger.py
@@ -7,9 +7,13 @@ import pickle as p
 
 class Logger:
     def __init__(self):
-        self.acid = []
+        self.acid_all = []
+        self.acid_activate = []
         self.data = {}
         self.enable = False
+        self.dt = 1.0
+        self.directory = "output/"
+        self.filename = "data_logging"
 
     def log(self, acid, flag):
         # find if acid exists
@@ -17,18 +21,19 @@ class Logger:
             raise ValueError('acid not found')
 
         # Enable or disable logging for acid
-        if flag is True:
-            self.acid.append(acid)
+        if flag:  # TRUE
+            self.acid_all.append(acid)
+            self.acid_activate.append(acid)
             self.data[acid] = []
-        if flag is False:
-            if acid in self.acid:
-                self.acid.remove(acid)
+        else:  # FALSE
+            if acid in self.acid_activate:
+                self.acid_activate.remove(acid)
             else:
                 print("Cannot remove acid, not added.")
 
     def update(self):
         if self.enable:
-            for ac in self.acid:
+            for ac in self.acid_activate:
                 idx = traf.id2idx(ac)
                 self.data[ac].append([sim.simt, traf.lat[idx], traf.lon[idx], traf.alt[idx], traf.hdg[idx],
                                       traf.gs[idx], traf.M[idx], traf.tas[idx], traf.perf.mass])
@@ -37,20 +42,20 @@ class Logger:
         pass
 
     def reset(self):
-        self.acid = []
+        self.acid_all = []
+        self.acid_activate = []
         self.data = {}
         self.enable = False
 
     def save(self):
         results = {}
-        for ac in self.acid:
+        for ac in self.acid_all:
             df = pd.DataFrame(self.data[ac], columns=['time', 'lat', 'lon', 'alt', 'heading', 'gs', 'M', 'tas', 'mass'])
             results[ac] = df
-        p.dump(results, open("output/data.p", "wb"))
+        p.dump(results, open(self.directory + self.filename + ".p", "wb"))
 
 
 logger = Logger()
-
 
 # Initialization function of your plugin. Do not change the name of this
 # function, as it is the way BlueSky recognises this file as a plugin.
@@ -68,7 +73,7 @@ def init_plugin():
         # Update interval in seconds. By default, your plugin's update function(s)
         # are called every timestep of the simulation. If your plugin needs less
         # frequent updates provide an update interval.
-        'update_interval': 1,
+        'update_interval': logger.dt,
 
         # The update function is called after traffic is updated. Use this if you
         # want to do things as a result of what happens in traffic. If you need to
@@ -90,29 +95,55 @@ def init_plugin():
         # The command name for your function
         'FLOGG_ADD': [
             # A short usage string. This will be printed if you type HELP <name> in the BlueSky console
-            'FLOGG_ADD [acid] ON/OFF',
+            'FLOGG_ADD acid ON/OFF',
 
             # A list of the argument types your function accepts. For a description of this, see ...
-            '[txt, onoff]',
+            'txt, onoff',
 
             # The name of your function in this plugin
             add,
 
             # a longer help text of your function.
-            'Print something to the bluesky console based on the flag passed to MYFUN.'],
+            'Turn on/off the logger for a specific acid'],
 
         'FLOGG_TOGGLE': [
             # A short usage string. This will be printed if you type HELP <name> in the BlueSky console
             'FLOGG_TOGGLE ON/OFF',
 
             # A list of the argument types your function accepts. For a description of this, see ...
-            '[onoff]',
+            'onoff',
 
             # The name of your function in this plugin
             toggle,
 
             # a longer help text of your function.
-            'Print something to the bluesky console based on the flag passed to MYFUN.']
+            'Turn on or off the main logger'],
+
+        'FLOGG_INTERVAL_S': [
+            # A short usage string. This will be printed if you type HELP <name> in the BlueSky console
+            'FLOGG_INTERVAL_S FLOAT',
+
+            # A list of the argument types your function accepts. For a description of this, see ...
+            '[float]',
+
+            # The name of your function in this plugin
+            interval,
+
+            # a longer help text of your function.
+            'Change the flogger update interval. Default is 1 second.'],
+
+        'FLOGG_FILENAME': [
+            # A short usage string. This will be printed if you type HELP <name> in the BlueSky console
+            'FLOGG_FILENAME [txt]',
+
+            # A list of the argument types your function accepts. For a description of this, see ...
+            '[txt]',
+
+            # The name of your function in this plugin
+            filename,
+
+            # a longer help text of your function.
+            'Change the flogger filename. Default is data_logging.']
     }
 
     # init_plugin() should always return these two dicts.
@@ -122,10 +153,23 @@ def init_plugin():
 # Other functions of your plugin
 def add(acid, flag=True):
     logger.log(acid, flag)
-    return flag, 'logger active'
+    if flag:
+        return acid, 'logger active'
+    else:
+        return acid, 'logger inactivate'
 
 
 def toggle(flag):
-    if logger.enable is True and flag is False:  # switch off
+    if logger.enable is True and flag is False:  # switch off and save
         logger.save()
+        return flag, 'logging saved'
     logger.enable = flag
+    return flag, 'logging started'
+
+def interval(dt=1.0):
+    logger.dt = dt
+    return dt, 'logging interval changed'
+
+def filename(filename="data_logging"):
+    logger.filename = filename
+    return filename, 'logging filename changed'
